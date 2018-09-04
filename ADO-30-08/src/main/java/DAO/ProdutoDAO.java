82b5ad84390d33b69a0202df25e7d6262e5aa023
angelo.xavier <angelo.xavier@IFTBSNBKL081487.interfile.com.br>
2018-09-04 11:05:32 -0300

    Assinatura dos métodos necessarios, esqueleto feito.
    Completar ajustes e realizar teste.
    
    Angelo PI3.

--- ADO-30-08/src/main/java/DAO/ProdutoDAO.java
+++ ADO-30-08/src/main/java/DAO/ProdutoDAO.java
@@ -1,66 +1,132 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import Connection.Conexao;
-import Data.ProdutoData;
-import java.sql.Statement;
+import DATA.ProdutoDATA;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author lucas.ssantos57
- */
 public class ProdutoDAO {
 
-    public void incluirProduto(ProdutoData p) throws ClassNotFoundException, SQLException {
+    public void incluirProduto(ProdutoDATA p) throws ClassNotFoundException, SQLException {
+        try {
+            String querySql = "INSERT INTO PRODUTO (NOME,DESCRICAO,PRECO_COMPRA,PRECO_VENDA,QUANTIDADE) VALUES (?,?,?,?,?)";
 
-        String querySql = "INSERT INTO PRODUTO (NOME,DESCRICAO,PRECO_COMPRA,PRECO_VENDA,QUANTIDADE) VALUES (?,?,?,?,?)";
+            //Cria conexão com banco de dados
+            Connection connection = new Conexao().obterConexao();
+
+            //Prepara a query
+            PreparedStatement pstmt = connection.prepareStatement(querySql);
+            //Captura campos e seta na query
+            pstmt.setString(1, p.getNome());
+            pstmt.setString(2, p.getDescricao());
+            pstmt.setFloat(3, p.getPrecoCompra());
+            pstmt.setFloat(4, p.getPrecoVenda());
+            pstmt.setInt(5, p.getQuantidade());
+            //pstmt.setDate(6, p.getDataCadastro());
+
+            pstmt.executeUpdate();
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Erro ao cadastrar");
+        }
+
+    }
+
+    public void consultarProduto() {
+        try {
+            /*
+            
+            PENDENTE
+            
+             */
+
+            String querySql = "";
+
+            //Cria conexão com banco de dados
+            Connection connection = new Conexao().obterConexao();
+
+            //Prepara a query
+            PreparedStatement pstmt = connection.prepareStatement(querySql);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Erro ao consultar o produto");
+        }
+    }
+
+    public void alterarProduto() {
+        try {
+            String querySql = "";
+
+            //Cria conexão com banco de dados
+            Connection connection = new Conexao().obterConexao();
+
+            //Prepara a query
+            PreparedStatement pstmt = connection.prepareStatement(querySql);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Erro ao alterar o produto");
+        }
+
+    }
+
+    public void excluirProduto(Integer id) throws ClassNotFoundException, SQLException {
+        try {
+            String querySql = "DELETE FROM PRODUTO WHERE ID = ?";
+
+            //Cria conexão com banco de dados
+            Connection connection = new Conexao().obterConexao();
+
+            //Prepara a query
+            PreparedStatement pstmt = connection.prepareStatement(querySql);
+
+            //Captura campos e seta na query
+            pstmt.setInt(1, id);
+
+            pstmt.executeUpdate();
+
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Erro ao excluir");
+        }
+
+    }
+
+    public List<ProdutoDATA> exibeProdutosDisponivelParaAlterar() throws SQLException, ClassNotFoundException {
+
+        String querySql = "SELECT * FROM PRODUTO";
 
         //Cria conexão com banco de dados
         Connection connection = new Conexao().obterConexao();
 
         //Prepara a query
         PreparedStatement pstmt = connection.prepareStatement(querySql);
-        //Captura campos e seta na query
-        pstmt.setString(1, p.getNome());
-        pstmt.setString(2, p.getDescricao());
-        pstmt.setFloat(3, p.getPrecoCompra());
-        pstmt.setFloat(4, p.getPrecoVenda());
-        pstmt.setInt(5, p.getQuantidade());
-        //pstmt.setDate(6, p.getDataCadastro());
 
-        pstmt.executeUpdate();
-        connection.close();
+        // Cria uma lista de produtos
+        List<ProdutoDATA> produtosList = new ArrayList<>();
 
-    }
+        // Executa comando no banco e retorna em 'rs' o resultado
+        ResultSet rs = pstmt.executeQuery(querySql);
 
-    public void consultarProduto() {
+        //para percorrer o resultset, faca:
+        while (rs.next()) {
 
-    }
+            ProdutoDATA p = new ProdutoDATA();
+                        
+            p.setId(rs.getInt("ID"));
+            p.setNome(rs.getString("NOME"));
+            p.setDescricao(rs.getString("DESCRICAO"));
+            p.setPrecoCompra(rs.getFloat("PRECO_COMPRA"));
+            p.setPrecoVenda(rs.getFloat("PRECO_VENDA"));
+            p.setQuantidade(rs.getInt("QUANTIDADE"));
+            p.setDataCadastro(rs.getDate("DT_CADASTRO"));
 
-    public void alterarProduto() {
-
-    }
-
-    public void excluirProduto(Integer id) throws ClassNotFoundException, SQLException {
-        try{
-        Connection conn = new Conexao().obterConexao();
-        Statement st = conn.createStatement();
-        st.executeUpdate("DELETE FROM PRODUTO WHERE (id=?)");
-        conn.close();
-        }catch(Exception e){
-            System.out.println("Erro ao excluir");
+            produtosList.add(p);
         }
+
+        return produtosList;
     }
 
 }
---
+++ ADO-30-08/src/main/java/Main/Principal.java
@@ -0,0 +1,162 @@
+package Main;
+
+import DAO.ProdutoDAO;
+import DATA.ProdutoDATA;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    public void main(String[] args) throws ClassNotFoundException, SQLException {
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("--------------------");
+            System.out.println("1 - LISTAR PRODUTOS");
+            System.out.println("2 - INCLUIR PRODUTOS");
+            System.out.println("3 - EXCLUIR PRODUTOS");
+            System.out.println("4 - ALTERAR PRODUTOS");
+            System.out.println("--------------------");
+
+            System.out.println("Digite a opção desejada: ");
+            int opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1: {
+                    listarProdutos();
+                }
+                case 2: {
+                    incluirProduto();
+                }
+                case 3: {
+                    excluirProduto();
+                }
+                case 4: {
+                    alterarProduto();
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.println("Erro main()");
+        }
+
+    }
+
+    private static void incluirProduto() throws ClassNotFoundException, SQLException {
+        try {
+            Scanner sc = new Scanner(System.in);
+            ProdutoDATA p = new ProdutoDATA();
+
+            System.out.println("Digite o nome do produto: ");
+            String Nome = sc.next();
+            p.setNome(Nome);
+
+            System.out.println("Digite a descrição do produto: ");
+            String Descricao = sc.next();
+            p.setDescricao(Descricao);
+
+            System.out.println("Digite o preço de compra do produto: ");
+            float PrecoCompra = sc.nextFloat();
+            p.setPrecoCompra(PrecoCompra);
+
+            System.out.println("Digite o preço de venda do produto: ");
+            float PrecoVenda = sc.nextFloat();
+            p.setPrecoVenda(PrecoVenda);
+
+            System.out.println("Digite a quantidade do produto: ");
+            int Quantidade = sc.nextInt();
+            p.setQuantidade(Quantidade);
+
+            ProdutoDAO pDAO = new ProdutoDAO();
+            pDAO.incluirProduto(p);
+        } catch (Exception e) {
+            System.out.println("Erro excluirProduto()");
+        }
+
+    }
+
+    private static void excluirProduto() throws ClassNotFoundException, SQLException {
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Digite o ID do produto: ");
+            Integer id = sc.nextInt();
+
+            ProdutoDAO pDAO = new ProdutoDAO();
+            pDAO.excluirProduto(id);
+        } catch (Exception e) {
+            System.out.println("Erro excluirProduto()");
+        }
+
+    }
+
+    private static void listarProdutos() throws ClassNotFoundException, SQLException {
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            ProdutoDAO pDAO = new ProdutoDAO();
+
+            /*
+
+            PENDENTE.
+
+             */
+        } catch (Exception e) {
+            System.out.println("Erro listarProdutos()");
+        }
+
+    }
+
+    private static void alterarProduto() throws ClassNotFoundException, SQLException {
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            ProdutoDAO pDAO = new ProdutoDAO();
+
+            // Para exibir os produtos cadastrados.
+            exibeProdutosDisponivelParaAlterar();
+
+            System.out.println("Digite o ID do produto a ser alterado: ");
+            Integer id = sc.nextInt();
+            
+            /*
+            
+            Completar o método
+            
+            */
+        } catch (Exception e) {
+            System.out.println("Erro alterarProduto()");
+        }
+
+    }
+
+    private static void exibeProdutosDisponivelParaAlterar() throws ClassNotFoundException, SQLException {
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            ProdutoDAO pDAO = new ProdutoDAO();
+
+            List<ProdutoDATA> produtosList = pDAO.exibeProdutosDisponivelParaAlterar();
+
+            
+            /* Arrumar a impressão */
+            for (ProdutoDATA p : produtosList) {
+                System.out.print(p.getId());  
+                System.out.print(p.getNome()); 
+                System.out.print(p.getDescricao()); 
+                System.out.print(p.getPrecoCompra()); 
+                System.out.print(p.getPrecoVenda()); 
+                System.out.print(p.getQuantidade()); 
+                System.out.print(p.getDataCadastro()); 
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro listarProdutos()");
+        }
+
+    }
+
+}
